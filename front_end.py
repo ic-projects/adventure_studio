cadb79a42860b77e7171d9d653bf93c4577b2755
Jordan Spooner <jordan.spooner16@imperial.ac.uk>
2018-01-20 14:12:31 +0000

    Complete very basic front-end

--- front_end.py
+++ front_end.py
@@ -6,14 +6,34 @@ def load_file():
     """Load a JSON file and gather game info"""
     data = json.load(open('example.json'))
 
+    # Start location
     start = data['start']
 
+    # Locations
     locations = {}
-    for loc in data['locations']:
-        locations[loc['id']] = loc
+    for locn in data['locations']:
+        locations[locn['id']] = locn
 
-    print(locations)
+    # Events
+    events = {}
+    for event in data['events']:
+        events[event['id']] = event
+
+    # Objects
+    objects = {}
+    for obj in data['objects']:
+        objects[obj['id']] = obj
+
+    # Bools
+    bools = data['bools']
+
+    # Bools
+    ints = data['ints']
+
+    # Bools
+    strings = data['strings']
+
+    return (start, locations, events, objects, bools, ints, strings)
 
 if __name__ == "__main__":
     load_file()
-    
