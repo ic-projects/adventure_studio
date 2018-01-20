a37100ae97ff3b365170631ec151d0743e9e9399
mfournial <mayeul.fournial@outlook.com>
2018-01-20 17:52:19 +0000

    Adds direction class

--- location.py
+++ location.py
@@ -11,18 +11,29 @@ class LocationBuilder:
     
     def with_objects(self, objects):
         self.objects = objects
+
+    def with_direction(self, direction):
+        self.direction = direction
     
     def build(self):
         return Location(self.id, self.description, self.always,
-                        self.choices, self.objects)
+                        self.choices, self.objects, self.direction)
+
+class Direction:
+    def __init__(self, n, e, s, w):
+        self.north = n
+        self.east = e
+        self.south = s
+        self.west = w
 
 class Location:
-    def __init__(self, name, description, always, choices, objects):
+    def __init__(self, name, description, always, choices, objects, direction):
         self.name        = name
         self.description = description
         self.always      = always
         self.choices     = choices
         self.objects     = objects
+        self.direction = direction
 
     # Post - after returning from enter we should be able to call self.nextLoc()
     def enter(self):
